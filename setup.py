87077b4b66cdebbb52e2419f798aa268fed5bb6a
HQupgradeHQ <18361586+HQupgradeHQ@users.noreply.github.com>
2017-10-19 08:09:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup (name = 'fake_typing',
-    install_requires =['keyboard==0.11.0']
+    install_requires =['keyboard==0.11.0'],
     version ='1',
     dependency_links = ['git+https://github.com/boppreh/keyboard@suppress#egg=keyboard-0.11.0'],
     scripts =['script.py'],
