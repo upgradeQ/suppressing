d3a701e5b2c538b889f3eae9dd5aafac385d4d21
FooBar <18361586+HQupgradeHQ@users.noreply.github.com>
2017-10-19 08:27:47 +0300

    dependency problem

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 setup (name = 'fake_typing',
-    install_requires =['keyboard==0.11.0']
+    install_requires =['keyboard==0.11.0+git.3483c57'],
     version ='1',
-    dependency_links = ['git+https://github.com/boppreh/keyboard@suppress#egg=keyboard-0.11.0'],
+    dependency_links =['https://github.com/boppreh/keyboard/archive/3483c578cbd9105aae831bf1a47572b4d5bd5aff.zip#egg=keyboard-0.11.0+git.3483c57'],
+    #dependency_links = ['https://github.com/boppreh/keyboard@suppress#egg=keyboard-0.11.0'],
     scripts =['script.py'],
 )
