92e414da60dd442bfbee4ddaeaec2a5e5c2550f8
HQupgradeHQ <18361586+HQupgradeHQ@users.noreply.github.com>
2017-10-21 21:48:59 +0300

    updated right branch path

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup
 setup (name = 'fake_typing',
 
-    install_requires =['keyboard==0.11.0+git5196a01'],
+    install_requires =['keyboard==0.11.0+git8a7df3d'],
     version ='1.1',
-    dependency_links = ['https://github.com/HQupgradeHQ/keyboard/archive/5196a013bcc304cde509880ea9666fbde2ee081a.zip#egg=keyboard-0.11.0+git5196a01'],
-   # dependency_links =['https://github.com/boppreh/keyboard/archive/3483c578cbd9105aae831bf1a47572b4d5bd5aff.zip#egg=keyboard-0.11.0+git.3483c57'],
-    
+    dependency_links = ['https://github.com/HQupgradeHQ/keyboard/archive/8a7df3d29d58f2b873a288722b3935382819fb0d.zip#egg=keyboard-0.11.0+git8a7df3d'],
     scripts =['script.py'],
 )
