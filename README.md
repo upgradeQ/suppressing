ec096ab1334762e99ac5a9ed5b65cea1c446082c
FooBar <18361586+HQupgradeHQ@users.noreply.github.com>
2017-11-04 16:37:13 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Global key presses replace 
+This script uses text from data.txt and when you type random keystrokes, it will appear as one of chars from that file.
+[![awesome](https://img.shields.io/badge/awesome-typing-ff69b5.svg)](https://github.com/HQupgradeHQ/suppressing)
+![](https://img.shields.io/badge/linux-failed-red.svg)
+
+# Installation 
+Clone or download this repo. Then run `python setup.py install` or           
+`pip install  git+git://github.com/HQupgradeHQ/suppressing.git --process-dependency-links`        
+ 
+# Usage 
+- Start suppressing  <kbd>Ctrl</kbd> + <kbd>3</kbd> 
+- Stop  <kbd>Ctrl</kbd> + <kbd>4</kbd> 
+
+# Branches
+- ![Block keyboard ability](https://github.com/HQupgradeHQ/suppressing/tree/block) press hotkey to block keyboard ( currently no way undo)
+
+- ![Hotkey support](https://github.com/HQupgradeHQ/suppressing/tree/hotkeys) e.g `<Alt+Enter>`,`<ctrl+alt+del>` , don't forget to add `<>`
+
+# Demo
+![](https://raw.githubusercontent.com/HQupgradeHQ/suppressing/master/demo.gif)
+
---
+++ script.py
@@ -0,0 +1,83 @@
+import keyboard
+#generator
+def sup():
+    for one_iteration in data:
+        yield  one_iteration
+#weird function name 
+s888 = sup()
+
+with open('data.txt', 'r') as myfile:
+    data=myfile.read()
+
+def do():
+    #bind keys
+    keyboard.add_hotkey('q',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('w',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('e',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('r',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('t',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('y',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('u',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('i',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('o',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('p',lambda:keyboard.write(str(next(s888))))
+
+
+    keyboard.add_hotkey('a',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('s',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('d',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('f',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('g',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('h',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('j',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('k',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('l',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey(';',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('\'',lambda:keyboard.write(str(next(s888))))
+
+    keyboard.add_hotkey('z',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('x',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('c',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('v',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('b',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('n',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('m',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey(',',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('.',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('/',lambda:keyboard.write(str(next(s888))))
+
+    keyboard.add_hotkey('[',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey(']',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('1',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('2',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('3',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('4',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('5',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('6',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('7',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('8',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('9',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('0',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('-',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('=',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey(' ',lambda:keyboard.write(str(next(s888))))
+    keyboard.add_hotkey('\\',lambda:keyboard.write(str(next(s888))))
+
+start=False   
+while True:
+    if start is True:
+         keyboard.wait('Ctrl+3')
+         do()
+         keyboard.wait('Ctrl+4')
+         keyboard.unhook_all()
+         continue
+    #control + 3 to start "typing" control + 4 to stop    
+    keyboard.wait('Ctrl+3')
+    do()
+    keyboard.wait('Ctrl+4')
+    keyboard.unhook_all()
+    #start again
+    start = True
+    continue
+    #to wait ctrl+3
+
